3f86d237e3c3d1a71aba26887e89aeca6d0ac8b0
Mikel Santos <msantos@wolfram.com>
2016-01-26 17:05:48 -0600

    gulpfile updates/fixes update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,70 @@
 'use strict';
 
-var argv        = require('yargs').argv;
-var browserSync = require('browser-sync').create();
-var concat      = require('gulp-concat');
-var gulp        = require('gulp');
-var minifyCss   = require('gulp-minify-css');
-var modRewrite  = require('connect-modrewrite');
-var sass        = require('gulp-sass');
-var sourcemaps  = require('gulp-sourcemaps');
-var ts          = require('gulp-typescript');
-var uglify      = require('gulp-uglify');
+var argv        = require('yargs').argv,
+    bower       = require('gulp-bower'),
+    browserSync = require('browser-sync').create(),
+    concat      = require('gulp-concat'),
+    cssnano     = require('gulp-cssnano'),
+    gulp        = require('gulp'),
+    gulpif      = require('gulp-if'),
+    jshint      = require('gulp-jshint'),
+    modRewrite  = require('connect-modrewrite'),
+    sass        = require('gulp-sass'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    ts          = require('gulp-typescript'),
+    uglify      = require('gulp-uglify');
 
 var nosourcemaps = argv.nosourcemaps;
 if (nosourcemaps)
    process.stdout.write("-- generating static files with no sourcemaps -- \n");
+   
+var cssPaths  = ['css/*.scss', 'css/*.css'],
+    jsPaths   = ['js/**/*.ts', '!js/dist/*.js'],
+    htmlPaths = ['*.html', 'templates/**/*.html'],
+    templates = ['templates/**/*.html'];
 
 // combine and compile css with sourcemaps
 gulp.task('css', function () {
-   if (nosourcemaps) {
-      return gulp.src(['css/*.css', 'css/sass/*.scss'])
-         .pipe(sass())
-         .pipe(minifyCss())
-         .pipe(concat('all.min.css'))
-         .pipe(gulp.dest('css/dist/'))
-         .pipe(browserSync.stream());
-   }
-   else {
-      return gulp.src(['css/*.css', 'css/sass/*.scss'])
-         .pipe(sourcemaps.init())
-         .pipe(sass())
-         .pipe(minifyCss())
-         .pipe(concat('all.min.css'))
-         .pipe(sourcemaps.write())
-         .pipe(gulp.dest('css/dist/'))
-         .pipe(browserSync.stream());
-   }
+   return gulp.src(cssPaths)
+      .pipe(gulpif(!nosourcemaps, sourcemaps.init()))
+      .pipe(sass())
+      .pipe(concat('all.min.css'))
+      .pipe(cssnano())
+      .pipe(gulpif(!nosourcemaps, sourcemaps.write()))
+      .pipe(gulp.dest('css/dist'))
+      .pipe(browserSync.stream());
 });
 
 // combine and compile js with sourcemaps
 gulp.task('js', function () {
-   if (nosourcemaps) {
-      return gulp.src(['js/**/*.ts', '!js/dist/*.js'])
-         .pipe(ts({
-            noImplicitAny: true,
-            outFile: 'js/dist/out.js',
-            declaration: false
-         }))
-         .pipe(uglify())
-         .pipe(concat('all.min.js'))
-         .pipe(gulp.dest('js/dist/'))
-         .pipe(browserSync.stream());
-   }
-   else {
-      return gulp.src(['js/**/*.ts', '!js/dist/*.js'])
-         .pipe(sourcemaps.init())
-         .pipe(ts({
-            noImplicitAny: true,
-            outFile: 'js/dist/out.js',
-            declaration: false
-         }))
-         .pipe(uglify())
-         .pipe(concat('all.min.js'))
-         .pipe(sourcemaps.write())
-         .pipe(gulp.dest('js/dist/'))
-         .pipe(browserSync.stream());
-   }
+   return gulp.src(jsPaths)
+      .pipe(gulpif(!nosourcemaps, sourcemaps.init()))
+      .pipe(ts({
+         noImplicitAny: true,
+         outFile: 'out.js',
+         declaration: false
+      }))
+      .pipe(uglify())
+      .pipe(concat('all.min.js'))
+      .pipe(gulpif(!nosourcemaps, sourcemaps.write()))
+      .pipe(gulp.dest('js/dist'))
+      .pipe(browserSync.stream());
+});
+
+// download bower dependencies if needed
+gulp.task('bowerInstall', function () {
+   return bower();
+});
+
+// jshint
+gulp.task('jshint', function () {
+   return gulp.src(jsPaths)
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // default gulp task to generate scripts & css
-gulp.task('default', ['css', 'js']);
+gulp.task('default', ['bowerInstall', 'css', 'jshint', 'js']);
 
 // run browser-sync server and watch for sass changes
 gulp.task('serve', ['css', 'js'], function () {
@@ -84,7 +80,7 @@ gulp.task('serve', ['css', 'js'], function () {
       }
   	});
 
-   gulp.watch('css/**/*.scss', ['css']).on('change', browserSync.reload);
-   gulp.watch(['js/**/*.js', 'js/**/*.ts', '!js/dist/*.js'], ['js']).on('change', browserSync.reload);
-   gulp.watch("**/*.html").on("change", browserSync.reload);
+   gulp.watch('css/**/*.scss', ['css']);
+   gulp.watch(jsPaths, ['js']).on('change', browserSync.reload);
+   gulp.watch(htmlPaths).on('change', browserSync.reload);
 });
