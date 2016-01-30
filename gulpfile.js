0b39f1d4e6850041f265d4c4d8ff459e75a3dac4
Mikel Santos <msantos@wolfram.com>
2016-01-30 10:51:41 -0600

    remove gsap generate and register minified html templates to angular cache

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,98 @@
 'use strict';
 
-var argv        = require('yargs').argv,
-    bower       = require('gulp-bower'),
-    browserSync = require('browser-sync').create(),
-    concat      = require('gulp-concat'),
-    cssnano     = require('gulp-cssnano'),
-    gulp        = require('gulp'),
-    gulpif      = require('gulp-if'),
-    jshint      = require('gulp-jshint'),
-    modRewrite  = require('connect-modrewrite'),
-    sass        = require('gulp-sass'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    ts          = require('gulp-typescript'),
-    uglify      = require('gulp-uglify');
+var argv          = require('yargs').argv,
+    bower         = require('gulp-bower'),
+    browserSync   = require('browser-sync').create(),
+    concat        = require('gulp-concat'),
+    cssnano       = require('gulp-cssnano'),
+    gulp          = require('gulp'),
+    gulpif        = require('gulp-if'),
+    jshint        = require('gulp-jshint'),
+    modRewrite    = require('connect-modrewrite'),
+    sass          = require('gulp-sass'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    templatecache = require('gulp-angular-templatecache'),
+    ts            = require('gulp-typescript'),
+    uglify        = require('gulp-uglify');
 
 var nosourcemaps = argv.nosourcemaps;
 if (nosourcemaps)
-   process.stdout.write("-- generating static files with no sourcemaps -- \n");
-   
-var cssPaths  = ['css/*.scss', 'css/*.css'],
-    jsPaths   = ['js/**/*.ts', 'js/**/*.js', '!js/dist/*.js'],
+    process.stdout.write("-- generating static files with no sourcemaps -- \n");
+
+var cssPaths = ['css/*.scss', 'css/*.css'],
+    jsPaths = ['js/**/*.ts', 'js/**/*.js', '!js/dist/*.js'],
     htmlPaths = ['*.html', 'templates/**/*.html'],
-    templates = ['templates/**/*.html'];
+    templatePaths = ['templates/**/*.html'];
 
-// combine and compile css with sourcemaps
+// combine and compile css
 gulp.task('css', function () {
-   return gulp.src(cssPaths)
-      .pipe(gulpif(!nosourcemaps, sourcemaps.init()))
-      .pipe(sass())
-      .pipe(concat('all.min.css'))
-      .pipe(cssnano())
-      .pipe(gulpif(!nosourcemaps, sourcemaps.write()))
-      .pipe(gulp.dest('css/dist'))
-      .pipe(browserSync.stream());
+    return gulp.src(cssPaths)
+        .pipe(gulpif(!nosourcemaps, sourcemaps.init()))
+        .pipe(sass())
+        .pipe(concat('all.min.css'))
+        .pipe(cssnano())
+        .pipe(gulpif(!nosourcemaps, sourcemaps.write()))
+        .pipe(gulp.dest('css/dist'))
+        .pipe(browserSync.stream());
 });
 
-// combine and compile js with sourcemaps
+// combine and compile js
 gulp.task('js', function () {
-   return gulp.src(jsPaths)
-      .pipe(gulpif(!nosourcemaps, sourcemaps.init()))
-      .pipe(ts({
-         noImplicitAny: true,
-         outFile: 'out.js',
-         declaration: false
-      }))
-      .pipe(uglify())
-      .pipe(concat('all.min.js'))
-      .pipe(gulpif(!nosourcemaps, sourcemaps.write()))
-      .pipe(gulp.dest('js/dist'))
-      .pipe(browserSync.stream());
+    return gulp.src(jsPaths)
+        .pipe(gulpif(!nosourcemaps, sourcemaps.init()))
+        .pipe(ts({
+            noImplicitAny: true,
+            outFile: 'out.js',
+            declaration: false
+        }))
+        .pipe(uglify())
+        .pipe(concat('all.min.js'))
+        .pipe(gulpif(!nosourcemaps, sourcemaps.write()))
+        .pipe(gulp.dest('js/dist'))
+        .pipe(browserSync.stream());
 });
 
-// download bower dependencies if needed
+// run $ bower install
 gulp.task('bowerInstall', function () {
-   return bower();
+    return bower();
 });
 
 // jshint
 gulp.task('jshint', function () {
-   return gulp.src(jsPaths)
-      .pipe(jshint())
-      .pipe(jshint.reporter('jshint-stylish'));
+    return gulp.src(jsPaths)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// register html templates to angular cache
+gulp.task('templates', function () {
+    return gulp.src(templatePaths)
+        .pipe(templatecache('templates.js', {
+            root: 'templates/',
+            standalone: true,
+            module: 'templates'
+        }))
+        .pipe(gulp.dest('js/'));
 });
 
 // default gulp task to generate scripts & css
-gulp.task('default', ['bowerInstall', 'css', 'jshint', 'js']);
+gulp.task('default', ['bowerInstall', 'css', 'jshint', 'templates', 'js']);
 
-// run browser-sync server and watch for sass changes
+// run browser-sync server and watch for changes
 gulp.task('serve', ['default'], function () {
-  	browserSync.init({
-      server: {
-         baseDir: "./",
-         middleware: [
-            // angular html5 mode apache rewrite
-            modRewrite([
-               '!\\.\\w+$ /index.html [L]'
-            ])
-         ]
-      }
-  	});
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            middleware: [
+                // angular html5 mode apache rewrite
+                modRewrite([
+                    '!\\.\\w+$ /index.html [L]'
+                ])
+            ]
+        }
+    });
 
-   gulp.watch('css/**/*.scss', ['css']);
-   gulp.watch(jsPaths, ['js']).on('change', browserSync.reload);
-   gulp.watch(htmlPaths).on('change', browserSync.reload);
+    gulp.watch('css/**/*.scss', ['css']);
+    gulp.watch(jsPaths, ['js']).on('change', browserSync.reload);
+    gulp.watch(htmlPaths, ['templates', 'js']).on('change', browserSync.reload);
 });
